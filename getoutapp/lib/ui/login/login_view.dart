0cd089b98b31e31c962d2db91b7422449c4ed9dc
Annihilator3180 <munozjacob48@yahoo.com>
2022-10-21 16:08:16 -0700

    Added Create an Account Button

--- getoutapp/lib/ui/login/login_view.dart
+++ getoutapp/lib/ui/login/login_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';        // new
-
+import 'package:firebase_auth/firebase_auth.dart'; // new
 
 class LoginView extends StatefulWidget {
-
   const LoginView({Key? key}) : super(key: key);
 
   @override
@@ -14,107 +12,96 @@ class _LoginView extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal[200],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+        backgroundColor: Colors.teal[200],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // title of our app
+                Text('Welcome to Get Out!',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 35)),
+                SizedBox(height: 10), // for space inbetween
+                Text('Please login to find events near you!',
+                    style: TextStyle(fontSize: 15)),
+                SizedBox(height: 30),
+                // email text feild
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.teal[50],
+                        border: Border.all(color: Colors.white)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 30.0),
+                      child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Email')),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                // password text feild
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.teal[50],
+                        border: Border.all(color: Colors.white)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 30.0),
+                      child: TextField(
+                          obscureText: true, // hides password when inputting
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Password')),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                // sign in button
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          color: Colors
+                              .grey), // if we want we can curve the borders within the decoration
+                      child: Center(
+                          child: Text('Sign In',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20))),
+                    )),
+                SizedBox(height: 20),
+                // create an account button
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          color: Colors
+                              .grey), // if we want we can curve the borders within the decoration
+                      child: Center(
+                          child: Text('Create An Account',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20))),
+                    )),
 
-              // title of our app
-              Text('Welcome to Get Out!',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 35)
-                  ),
-              SizedBox(height: 10), // for space inbetween
-              Text('Please login to find events near you!',
-                style: TextStyle(
-                  fontSize: 15)
-                  ),
-              SizedBox(height: 30),
-              // email text feild
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.teal[50],
-                    border: Border.all(color: Colors.white)
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 30.0),
-                    child: TextField(
-                      
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email' 
-                        
-                      )
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              // password text feild
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.teal[50],
-                    border: Border.all(color: Colors.white)
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 30.0),
-                    child: TextField(
-                      obscureText: true, // hides password when inputting
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password' 
-                        
-                      )
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              // sign in button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(color: Colors.grey),// if we want we can curve the borders within the decoration
-                  child: Center(
-                    child: Text(
-                      'Sign In',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20
-                      )
-                    )
-                  ),
+                // First time? Register Now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('First time here?'),
+                    Text(' Create account now',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ))
+                  ],
                 )
-              ),
-              SizedBox(height: 20),
-
-              // First time? Register Now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('First time here?'),
-                  Text(
-                    ' Create account now', 
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    )
-                  )
-                ],
-              )
-        
-        
-        
-          ],),
-        ),
-    ) );
+              ],
+            ),
+          ),
+        ));
   }
 }
