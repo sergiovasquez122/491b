03849727622f3d29a36901611118b9cc58595fd3
Annihilator3180 <munozjacob48@yahoo.com>
2022-11-01 22:20:19 -0700

    Added Reset Account View
    - Added a Event handler for Reset Account in main.dart.
    - Added a button to go back to Homepage

--- getoutapp/lib/main.dart
+++ getoutapp/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:getoutapp/ui/login/login_view.dart';
 import 'package:getoutapp/ui/createaccount/create_account_view.dart';
 import 'package:getoutapp/ui/homepage/homepage_view.dart';
+import 'package:getoutapp/ui/resetaccount/reset_account_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => const LoginView(),
         '/second': (context) => const CreateAccountView(),
         '/home': (context) => const HomePageView(),
+        '': (context) => const ResetAccountView(),
       },
       //home: const LoginView(), //CreateAccountView(),
     );
