6d208dc869c6995cc7113d54467e71c78f3ec60e
samanthaperezz <psamantha222@gmail.com>
2022-11-22 20:00:14 -0800

    Updated create event form UI
    Added a button at the end of the form and made the page look nicer.

--- getoutapp/lib/ui/createEvent/create_event_view.dart
+++ getoutapp/lib/ui/createEvent/create_event_view.dart
@@ -23,9 +23,12 @@ class _CreateEventViewState extends State<CreateEventView> {
 
 
    Widget build(BuildContext context) {
+    
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 230, 243, 242),
       appBar: AppBar(
-        title: Text('Create an Event'),
+        backgroundColor: Colors.teal[200]
+        //title: Text('Create an Event'),
       ),
       body: SingleChildScrollView(
         child: Form(
@@ -34,6 +37,18 @@ class _CreateEventViewState extends State<CreateEventView> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
             children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Text('Create an Event',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 35)),
+              ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 60.0, right: 45.0),
+                  child: Text('Please fill out this form to create your event.',
+                      style: TextStyle(fontSize: 15)),
+                ),
               // event name
               Padding(
                 padding: const EdgeInsets.all(30.0),
@@ -44,7 +59,21 @@ class _CreateEventViewState extends State<CreateEventView> {
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(20.0)),
                       borderSide: 
-                        BorderSide(color: Colors.grey, width: 0.0)
+                        BorderSide(color: Colors.grey, width: 2.5)
+                    )
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    //icon: Icon(Icons.app_registration),
+                    labelText: 'Location',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: 
+                        BorderSide(color: Colors.grey, width: 2.5)
                     )
                   ),
                 ),
@@ -58,7 +87,7 @@ class _CreateEventViewState extends State<CreateEventView> {
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(20.0)),
                       borderSide: 
-                        BorderSide(color: Colors.grey, width: 0.0)
+                        BorderSide(color: Colors.grey, width: 2.5)
                     )
                   ),
                 ),
@@ -72,7 +101,7 @@ class _CreateEventViewState extends State<CreateEventView> {
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(20.0)),
                       borderSide: 
-                        BorderSide(color: Colors.grey, width: 0.0)
+                        BorderSide(color: Colors.grey, width: 2.5)
                     )
                   ),
                 ),
@@ -90,11 +119,19 @@ class _CreateEventViewState extends State<CreateEventView> {
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(20.0)),
                       borderSide: 
-                        BorderSide(color: Colors.grey, width: 0.0)
+                        BorderSide(color: Colors.grey, width: 2.5)
                     )
                   ),
                 ),
-              ), 
+              ),
+
+              Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                      },
+                      child: const Text('Create'),
+                    )), 
 
 
             ],
